1f89e5d4e22299cf2c3276a3abaa62f3397b3fa1
David Bierbaum <David Bierbaum>
2016-11-03 15:16:22 -0700

    fixed HighwireAuthRequeste

--- HighwireAPIWrapper/Requests/BaseTypes/HighwireAuthRequest.cs
+++ HighwireAPIWrapper/Requests/BaseTypes/HighwireAuthRequest.cs
@@ -13,7 +13,7 @@ namespace HighwireAPIWrapper.Requests.BaseTypes
         private string Token { get; set; }
 
         internal HighwireAuthRequest(string appID, string secretKey, string token)
-            : base(eHttpMethod.Get, "auth")
+            : base(eHttpMethod.Post, "auth")
         {
             this.AppID = appID;
             this.SecretKey = secretKey;
@@ -30,6 +30,7 @@ namespace HighwireAPIWrapper.Requests.BaseTypes
             request.Proxy = null;
             request.KeepAlive = false;
             request.Method = this.HttpMethod.ToString().ToUpper();
+            request.ContentLength = 0; 
 
             if (HighwireAPI.DebugMode)
             {
